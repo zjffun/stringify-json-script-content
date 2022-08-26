4f5390a2ef80f76473926bd93318750ca504630c
Jufeng Zhang <zjffun@gmail.com>
2022-08-27 02:24:49 +0800

    test: add encode special chars test

--- tests/encode-json-script-content.ts
+++ tests/encode-json-script-content.ts
@@ -3,9 +3,9 @@ import { encodeJsonScriptContent } from "../src/index.js";
 
 describe("stringify-json-script-content", () => {
   it("should encode special chars", () => {
-    const jsonString = JSON.stringify({ foo: "bar<script/>" });
+    const jsonString = JSON.stringify({ foo: "bar<script><script/>" });
     expect(encodeJsonScriptContent(jsonString)).to.be.equal(
-      `{"foo":"bar\\u003Cscript\\u002F\\u003E"}`
+      `{"foo":"bar\\u003Cscript\\u003E\\u003Cscript\\u002F\\u003E"}`
     );
   });
 });
